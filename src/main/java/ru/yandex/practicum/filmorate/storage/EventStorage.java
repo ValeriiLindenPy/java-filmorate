2dfd2da379d50c6b3de9f1f725de25081b6eb472
Dinar Khazipov <khazipovdn@yandex.ru>
2024-12-09 00:42:39 +0300

    add-feed ver4

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.storage.mapper.EventRowMapper;
 import ru.yandex.practicum.filmorate.model.Event;
 
-import java.time.ZoneOffset;
 import java.util.List;
 
 @Repository
@@ -17,24 +16,11 @@ public class EventStorage {
 
     private static final String INSERT_EVENT_QUERY = """
     INSERT INTO EVENTS (id, user_id, event_type, operation, timestamp, entity_id)
-    VALUES (
-        ?, 
-        ?, 
-        (SELECT event_type FROM event_types WHERE name = ?), 
-        (SELECT operation FROM operations WHERE name = ?), 
-        ?, 
-        ?
-    )
+    VALUES (?, ?, (SELECT event_type FROM event_types WHERE name = ?), (SELECT operation FROM operations WHERE name = ?), ?, ?)
 """;
 
     private static final String GET_EVENTS_BY_USER_QUERY = """
-    SELECT 
-        e.id, 
-        e.user_id, 
-        et.name AS event_type, 
-        o.name AS operation, 
-        e.timestamp, 
-        e.entity_id
+    SELECT e.id, e.user_id, et.name AS event_type, o.name AS operation, e.timestamp, e.entity_id
     FROM EVENTS e
     JOIN event_types et ON e.event_type = et.event_type
     JOIN operations o ON e.operation = o.operation
@@ -43,13 +29,7 @@ public class EventStorage {
 """;
 
     private static final String GET_ALL_EVENTS_QUERY = """
-    SELECT 
-        e.id, 
-        e.timestamp, 
-        e.user_id, 
-        et.name AS event_type, 
-        o.name AS operation, 
-        e.entity_id
+    SELECT e.id, e.timestamp, e.user_id, et.name AS event_type, o.name AS operation, e.entity_id
     FROM EVENTS e
     JOIN event_types et ON e.event_type = et.event_type
     JOIN operations o ON e.operation = o.operation
